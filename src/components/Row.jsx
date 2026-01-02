130603d23fec362ed383b23a9fdb39148dbcc0ba
SETAYESH <setayeshazizi124@gmail.com>
2026-01-02 18:09:53 +0430

    personalinfo

---
+++ src/components/Row.jsx
@@ -0,0 +1,10 @@
+export default function Row({label, value}){
+    return(
+        <div className="row">
+             <span className="rowLabel"> {label} </span>
+             <span className="rowValue"> {value} </span>
+        </div>
+       
+    )
+
+}
