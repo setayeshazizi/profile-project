00cbcdc237a8447010f8d3d12004b2f3b992b60d
SETAYESH <setayeshazizi124@gmail.com>
2026-01-02 17:28:47 +0430

    replaces the default vite react starter content with a profile page layout.

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import avatarImage from './assets/aavatar.png'
+import ProfilePage from './components/ProfilePage'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const user ={
+    name: "razma azizi",
+    username: "@razmazizi",
+    avatarImage: avatarImage,
+    xp: 80,
+    lastActive: "Dec 29 2025",
+    streakDays: 2,
+    longestStreak: 5,
+    recentBadgesText: "No badges earned yet.",
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    personalInfo: {
+      Email:'setayeshazizi124@gmail.com',
+      Gender: 'fmail',
+      Country: 'Afghanistan',
+      "Province or State": 'Herat',
+      "Teaching Expreiance": "3",
+      "Date of Birth": "22/7/2007",
+      "Online Portfolio Link": "N/A",
+      "Linkedin Link": "Open the  Link",
+      Languages: "Farsi, English",
+      Bio: "God's plan",
+    },
+    accountDetails:{
+      Role: "Student",
+      Status: "Active",
+      Verification: "Verified",
+    }
+  }
+
+  return(
+    <div className='page'>
+      <ProfilePage user={user}></ProfilePage>
+
+    </div>
   )
 }
 
